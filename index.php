218d586fabd40658efa58d4ebb8ef8b9bae5c417
emma <emma.wishart@live.com.au>
2020-02-25 09:28:47 +0100

    connection is working, I forgot to push yesterday

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+ini_set('display_errors', "1");
+ini_set('display_startup_errors', "1");
+error_reporting(E_ALL);
+
+require "connection.php";
+
+$connection = openConnection();
+if ($connection){
+    echo "connected!!!";
+}
